e0c0ab2c23a1891a282f1135066456858dfbb221
IanKlek <ian.klek@gmail.com>
2021-07-28 08:01:48 +0100

    Grapql working

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+    hello: () => {
+        return 'Hello world!';
+    },
+};
 
 var app = express();
 
@@ -15,6 +31,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 app.use('/users', usersRouter);
 
 module.exports = app;
